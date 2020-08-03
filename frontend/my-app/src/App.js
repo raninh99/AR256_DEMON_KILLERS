34b39c405a04aa3f667964087f86f64447b45707
ashu <ayushgupta2305@gmail.com>
2020-08-03 10:35:16 +0530

    routes added

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -7,15 +7,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 
-
 import NavBar from './util/NavBar';
+
 
 import login from './pages/Login1';
 import signup from './pages/Signup';
+import prediction from './pages/Prediction';
 import logout from './pages/Logout';
-
 import forgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
+// import marketPlace from './pages/MarketPlace';
+// import Profile from './pages/profile';
+// import FAQ from './pages/FAQ';
+
+// import Buyer from './pages/buyerseller';
+// import Footer from './util/Footer';
 
 import Store from './store/store';
 import rootReducer from './rootReducer/rootReducer';
@@ -31,15 +37,19 @@ const  App = () => {
     <Store.Provider value={{state,dispatch}} >
       <div className="App">
       <Router>
-      <NavBar />
+        <NavBar />
         <Switch>
           {/* <Route exact path = '/home' component ={home} /> */}
           <Route path = '/login' component = {login} />
           <Route path = '/logout' component = {logout} />
           <Route path = '/signup' component = {signup} />
+          <Route path = '/prediction' component = {prediction} />
           <Route path = '/forgotPassword' component = {forgotPassword} />
           <Route exact path = '/' component = {Home} />
-          
+          {/* <Route path = '/marketPlace' component = {marketPlace} />
+          <Route path = '/FAQ' component = {FAQ}/>
+          <Route path = '/profile' component = {Profile} />
+          <Route path = '/buyer' component = {Buyer} /> */}
           {/* <Route path = '/logout' component = {logout} /> */}
           
         </Switch>
